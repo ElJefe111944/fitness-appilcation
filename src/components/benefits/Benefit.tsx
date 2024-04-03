a680d319cbd97b8cc24ace3a6b5f0cdfe2d583ff
James Farr <james.farr@jdplc.com>
2024-04-03 16:12:37 +0100

    Styling fix added to benefits border

--- src/components/benefits/Benefit.tsx
+++ src/components/benefits/Benefit.tsx
@@ -12,14 +12,14 @@ function Benefit({ icon, title, description, setSelectedPage }: Props) {
   return (
     <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
         <div className="mb-4 flex justify-center">
-            <div className="rounded-full border-2 border-gray-200 bg-primary-100 p-4">
+            <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
                 {icon}
             </div>
         </div>
         <h4 className="font-bold">{title}</h4>
         <p className="my-3">{description}</p>
         <AnchorLink
-            className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+            className='text-sm font-bold text-gray-500 underline hover:text-secondary-500'
             onClick={() => setSelectedPage(SelectedPage.ContactUs)}
             href={`#${SelectedPage.ContactUs}`}
         >

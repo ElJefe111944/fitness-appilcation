88b133514349198a8c3e9018e92ffa2255e81ab5
James Farr <james.farr@jdplc.com>
2024-04-03 18:16:04 +0100

    Styling fix added to Class component

--- src/components/classes/Class.tsx
+++ src/components/classes/Class.tsx
@@ -9,10 +9,10 @@ function Class({ name, description, image }: Props) {
   return (
     <li className="relative mx-5 in-line-block h-[380px w-[450px]">
         <div className="p-5 absolute z-30 flex h-[380px] flex-col items-center justify-center whitespace-normal bg-opacity-30 bg-black text-center text-white opacity-0 transition duration-500 hover:opacity-90">
-            <p>
+            <p className="text-2xl">
                 {name}
             </p>
-            <p>
+            <p className="mt-5">
                 {description}
             </p>
         </div>

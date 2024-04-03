66ab235f1b97519d86807ab512382e30c203ee0b
James Farr <james.farr@jdplc.com>
2024-04-03 18:33:43 +0100

    Contact us component created

--- src/components/classes/Class.tsx
+++ src/components/classes/Class.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 function Class({ name, description, image }: Props) {
   return (
-    <li className="relative mx-5 in-line-block h-[380px w-[450px]">
-        <div className="p-5 absolute z-30 flex h-[380px] flex-col items-center justify-center whitespace-normal bg-opacity-30 bg-black text-center text-white opacity-0 transition duration-500 hover:opacity-90">
+    <li className="relative mx-5 in-line-block h-[380px] w-[450px]">
+        <div className="p-5 absolute z-30 w-full flex h-[380px] flex-col items-center justify-center whitespace-normal bg-opacity-30 bg-black text-center text-white opacity-0 transition duration-500 hover:opacity-90">
             <p className="text-2xl">
                 {name}
             </p>

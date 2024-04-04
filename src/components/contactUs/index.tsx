9c72f733d860574dc9d4025067da3935cf63899b
James Farr <james.farr@jdplc.com>
2024-04-04 12:37:40 +0100

    Form added to Contact us template and styled

--- src/components/contactUs/index.tsx
+++ src/components/contactUs/index.tsx
@@ -10,6 +10,23 @@ interface Props {
 
 
 function ContactUs({ setSelectedPage }: Props) {
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmitHandler = async (e: any) => {
+    const isValid = await trigger();
+
+    if(!isValid){
+      e.preventDefault();
+
+
+    }
+  };
+
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
@@ -32,6 +49,86 @@ function ContactUs({ setSelectedPage }: Props) {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo officia, odio est cupiditate repudiandae asperiores. Ullam similique deserunt nesciunt repudiandae aut aliquid officia voluptas nulla nostrum rem vitae, minima dolor.
           </p>
         </motion.div>
+        {/* FORM AND IMAGE */}
+        <div className="mt-20 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.6 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 }
+            }}
+          >
+            <form 
+              target="_blank" 
+              onSubmit={onSubmitHandler}
+              method="POST"
+              action="https://formsubmit.co/jamesfarr1@sky.com"
+            >
+              {/* NAME */}
+               <input 
+               {...register('name', {
+                  required: true,
+                  maxLength: 100
+                })} 
+                className="w-full rounded-lg bg-primary-300 px-5 py-3 placeholder:text-white" 
+                type="text" 
+                name="name" 
+                placeholder="NAME"
+               />
+               {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" && "Max length is 100 char."}
+                </p>
+               )}
+              {/* EMAIL */}
+              <input 
+               {...register('email', {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })} 
+                className="mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder:text-white" 
+                type="email" 
+                name="email" 
+                placeholder="EMAIL"
+               />
+               {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" && "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+               )}
+               {/* MESSAGE */}
+               <textarea 
+               {...register('message', {
+                  required: true,
+                  maxLength: 2000
+                })} 
+                className="mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder:text-white" 
+                rows={4}
+                cols={50}
+                name="message" 
+                placeholder="MESSAGE"
+               />
+               {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" && "This field is required."}
+                  {errors.message.type === "maxLength" && "Max length is 2000 char."}
+                </p>
+               )}
+               <button 
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+               >
+                SUBMIT
+               </button>
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   )
